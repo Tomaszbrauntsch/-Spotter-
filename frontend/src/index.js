5cf3c41082fd29461a34182bad21f079f7829fc1
SophBr <sophie.breunig@yahoo.de>
2019-02-16 21:33:22 +0100

    Added a little bit of Text to a website that gives the user an output and lets him take a picture. I also added a little bit of css (needs more work). Next step is to make it interactive and add an if statement into the JavaScript that displays a result based on the persons picture. We also need to link the actual picture into the website and make the button work.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,3 +4,5 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+//we need to code here: if number is smaller than .5(?) display results: opt1; if not display results: opt2
