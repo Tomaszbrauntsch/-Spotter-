f8bddf7f9fa0b43aad7185d979c8c9648df4b5b3
tomnie <tomnt1997@gmail.com>
2019-02-16 00:01:39 -0500

    Add camera logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    let width = 320;    // We will scale the photo width to this
+    let height = 0;     // This will be computed based on the input stream
+  
+    let streaming = false;
+    let video = null;
+    let canvas = null;
+    let photo = null;
+    let startbutton = null;
+  
+  }
+
+  componentDidMount() {
+    this.video = document.getElementById('video');
+    this.canvas = document.getElementById('canvas');
+    this.photo = document.getElementById('photo');
+    this.startbutton = document.getElementById('startbutton');
+
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(function(stream) {
+      this.video.srcObject = stream;
+      this.video.play();
+    })
+    .catch(function(err) {
+        console.log("An error occurred! " + err);
+    });
+
+  }
+
+  clearPhoto() {
+    return;
+  }
+
+  takePicture() {
+    var context = this.canvas.getContext('2d');
+    if (this.width && this.height) {
+      this.canvas.width = this.width;
+      this.canvas.height = this.height;
+      context.drawImage(this.video, 0, 0, this.width, this.height);
+    
+      var data = this.canvas.toDataURL('image/png');
+      this.photo.setAttribute('src', data);
+    } else {
+      this.clearPhoto();
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <body>
+            <div class="camera">
+              <video id="video">Video stream not available.</video>
+              <button id="startbutton" onClick={() => { this.takePicture(); }}>Take photo</button>
+            </div>
+            <canvas id="canvas" />
+            <div class="output">
+              <img id="photo" alt="The screen capture will appear in this box." />
+            </div>
+          </body>
         </header>
       </div>
     );
@@ -26,3 +71,6 @@ class App extends Component {
 }
 
 export default App;
+
+
+
