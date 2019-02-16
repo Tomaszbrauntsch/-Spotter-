5f9a625c7e901e4ee4c647d6c96fa009b00c9fdb
tomnie <tomnt1997@gmail.com>
2019-02-16 16:00:33 -0500

    Combine frontend and server

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-//we need to code here: if number is smaller than .5(?) display results: opt1; if not display results: opt2
