cfaea789c4af1f467772059a86b2a763de15e6b1
tomnie <tomnt1997@gmail.com>
2019-02-16 19:06:32 -0500

    Add variable output depending on classifier

--- src/app.py
+++ src/app.py
@@ -1,13 +1,12 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template
 import json
 from watson_developer_cloud import VisualRecognitionV3
-import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return Response(open('test.html').read(), mimetype="text/html")
+    return render_template('index.html')
 
 # save the image as a picture
 @app.route('/images', methods=['POST'])
@@ -15,21 +14,26 @@ def image():
 
     i = request.files['image']  # get the image
     f = ('image.jpeg')
-    i.save('%s/%s' % ("images/", f))
+    i.save('%s' % f)
 
     visual_recognition = VisualRecognitionV3(
          version='2018-03-19',
          iam_apikey='KgyBkE_NeZ9WqPjJyrRQcDDdgIgupFUAP-4c9KQkwN_2'
      )
 
-    with open('images/image.jpeg', 'rb') as images_file:
+    with open('image.jpeg', 'rb') as images_file:
         classes = visual_recognition.classify(
             images_file,
             threshold='0.0',
          classifier_ids='DefaultCustomModel_626967875').get_result()
     print(json.dumps(classes, indent=2))
 
-    return Response("%s saved" % f)
+    if (classes['images'][0]['classifiers'][0]['classes'][0]['score']) > 0.5:
+        isTired = 'True'
+    else:
+        isTired = 'False'
+
+    return Response(isTired)
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
